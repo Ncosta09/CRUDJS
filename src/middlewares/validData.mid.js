c6c11e809351eeed163da36ed3496cdea12ac0f8
Ncosta09 <ncosta981@gmail.com>
2024-09-23 11:14:48 -0300

    productos finalizado

--- src/middlewares/validData.mid.js
+++ src/middlewares/validData.mid.js
@@ -1,6 +1,6 @@
 function validateProductData(req, res, next){
     try {
-        const data = req.body;
+        //const data = req.body;
         const { title, photo, category, price, stock } = req.body;
 
         if(!title || !photo || !category || !price || !stock){
@@ -8,7 +8,11 @@ function validateProductData(req, res, next){
             error.statusCode = 400;
 
             throw error;
-        }else{
+        }
+        if (typeof price !== 'number' || price < 0 || typeof stock !== 'number' || stock <= 0) {
+            return res.status(400).json({ message: "PRICE AND STOCK MUST BE A POSITIVE NUMBER!" });
+        }
+        else{
             return next();
         }
 
@@ -17,4 +21,29 @@ function validateProductData(req, res, next){
     }
 }
 
-export default validateProductData;
+function validateProductUpdateData(req, res, next){
+    try {
+        const { title, photo, category, price, stock } = req.body;
+
+        if(!title && !photo && !category && price == null && stock == null){
+            return res.status(400).json({ message: "AT LEAST ONE PROPERTY MUST BE PROVIDED TO UPDATE!" })
+        }
+        if(price !== undefined && (typeof price !== 'number' || price < 0)){
+            return res.status(400).json({ message: "PRICE MUST BE A POSITIVE NUMBER!" })
+        }
+        if(stock !== undefined && (typeof stock !== 'number' || stock <= 0)){
+            return res.status(400).json({ message: "STOCK MUST BE A POSITIVE NUMBER!" })
+        }
+        else{
+            return next();
+        }
+        
+    } catch (error) {
+        
+    }
+}
+
+export {
+    validateProductData, 
+    validateProductUpdateData
+};
--- src/routers/api/products.api.js
+++ src/routers/api/products.api.js
@@ -6,15 +6,18 @@ import {
     getProduct,
     updateProduct 
 } from "../../controllers/products.controller.js";
-import validateProductData from "../../middlewares/validData.mid.js";
+import { 
+    validateProductData,
+    validateProductUpdateData 
+} from "../../middlewares/validData.mid.js";
 
 const productsRouter = Router();
 
 //productsRouter.get("/", index);
 productsRouter.get("/", getAllProducts);
 productsRouter.get("/:pid", getProduct);
-productsRouter.post("/", validateProductData , createProduct);
-productsRouter.put("/:pid", updateProduct);
+productsRouter.post("/", validateProductData, createProduct);
+productsRouter.put("/:pid", validateProductUpdateData, updateProduct);
 productsRouter.delete("/:pid", deleteProduct);
 
 export default productsRouter;
